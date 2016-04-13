7c03803cdc393ef886b8baef3f80b613dc6d886c
liumin <970568830@qq.com>
2016-04-13 20:12:23 +0800

    代码提交信息

---
+++ js/data.js
@@ -0,0 +1,34 @@
+// 初始化并连接到数据库github
+Bmob.initialize("a69c3dcc064b8d85683648d39c2567e7", "65def0c197d589d10f319dd02b93a351");
+// var Github = Bmob.Object.extend("Github");
+// var github = new Github();
+// github.set("home", 0);
+// github.save(null, {
+//     success: function(object) {
+//         alert("create object success, object id:" + object.id);
+//     },
+//     error: function(model, error) {
+//         alert("create object fail");
+//     }
+// });
+
+
+ // var github = Bmob.Object.extend("github");
+ //    var query = new Bmob.Query(github);
+ //    query.get("e10be4ea4a", {
+ //      success: function(object) {
+ //        // The object was retrieved successfully.
+ //        object.set("score", 1338);
+ //        object.save(null, {
+ //          success: function(objectUpdate) {
+ //            alert("create object success, object score:"+objectUpdate.get("score"));
+ //          },
+ //          error: function(model, error) {
+ //            alert("create object fail");
+ //          }
+ //        });
+ //      },
+ //      error: function(object, error) {
+ //        alert("query object fail");
+ //      }
+ //    });
